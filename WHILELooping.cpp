35bb7570a9b9cc38807447c77f26289bb25f9ddf
nachool <nachma.olivia1394@gmail.com>
2024-03-19 18:31:58 +0700

    while looping

--- WHILELooping.cpp
+++ WHILELooping.cpp
@@ -1,1 +1,19 @@
 #include <iostream>
+using namespace std;
+
+int main() {
+    int bilangan;
+
+    srand(time(0));
+
+    bilangan = rand() % 10;
+
+    while (bilangan > 4) 
+    {
+        cout << "Bilangan lebih dari 4" << endl;
+        bilangan = rand() % 10;
+        cout << "Bilangannya =" << bilangan << endl;
+    }
+
+    cout << "Bilangan terakhir =" << bilangan << endl;
+}
